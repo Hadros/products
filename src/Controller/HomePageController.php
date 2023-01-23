a60d7356ff0043d73f5358045ef353391555cbc8
Slava Dergachyov <mr.hadros@gmail.com>
2023-01-23 15:21:33 +0300

    REMOVED: debug code.

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -12,9 +12,6 @@ class HomePageController extends AbstractController
     #[Route('/', name: 'home_page')]
     public function index(HttpClientInterface $client): Response
     {
-
-        $response = $client->request('GET', 'https://127.0.0.1:8000/api/products');
-        $decoded_response = json_decode($response->getContent(), true);
         return new Response('Hello, World!');
     }
 }
